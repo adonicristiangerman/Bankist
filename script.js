0b252484c24a8c8165369eaf077d23fab6374977
Adoni Cristian <32308799+adonicristiangerman@users.noreply.github.com>
2025-09-02 23:15:08 -0400

    Fixed a bug with movement dates

--- script.js
+++ script.js
@@ -77,14 +77,17 @@ const inputClosePin = document.querySelector('.form__input--pin');
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort
-    ? acc.movements.slice().sort((a, b) => a - b)
-    : acc.movements;
+  const combinedMovsDates = acc.movements.map((mov, i) => {
+    return { movement: mov, movDate: acc.movementsDates.at(i) };
+  });
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+  if (sort) combinedMovsDates.sort((a, b) => a.movement - b.movement);
 
-    const date = new Date(acc.movementsDates[i]);
+  combinedMovsDates.forEach(function (obj, i) {
+    const { movement, movDate } = obj;
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(movDate);
     const day = `${date.getDate()}`.padStart(2, 0);
     const month = `${date.getMonth() + 1}`.padStart(2, 0);
     const year = date.getFullYear();
@@ -95,7 +98,9 @@ const displayMovements = function (acc, sort = false) {
       i + 1
     } ${type}</div>
           <div class="movements__date">${displayDate}</div>
-          <div class="movements__value">RD$${Math.abs(mov).toFixed(2)}</div>
+          <div class="movements__value">RD$${Math.abs(movement).toFixed(
+            2
+          )}</div>
         </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
